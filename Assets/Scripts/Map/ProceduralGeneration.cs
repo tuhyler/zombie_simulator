d34bd65a5391b14ba8ac8a4025ede5c62638eafa
Tyler Hansen -Laptop <tuhyler@gmail.com>
2022-09-11 23:07:52 -0600

    started mountain range generation algorithm
    Former-commit-id: 86247ef836054fd50ceebbf397f51e6365bc99fc
    Former-commit-id: 33163b8f8d3e7394ed8a4f3fac18cce92e0df820

--- Assets/Scripts/Map/ProceduralGeneration.cs
+++ Assets/Scripts/Map/ProceduralGeneration.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Mathematics.math;
 
 public class ProceduralGeneration
 {
@@ -45,10 +46,10 @@ public class ProceduralGeneration
     //    return path;
     //}
 
-    public static HashSet<float> PerlinNoiseGenerator(int width, int height, float scale, int octaves, float persistance, 
+    public static List<float> PerlinNoiseGenerator(int width, int height, float scale, int octaves, float persistance, 
         float lacunarity)
     {
-        HashSet<float> noiseList = new();
+        List<float> noiseList = new();
         
         if (scale <= 0)
             scale = 0.0001f;
@@ -86,12 +87,27 @@ public class ProceduralGeneration
     }
 
     public static List<float> PerlinNoiseGenerator(List<Vector3Int> positions, float scale, int octaves, float persistance,
-        float lacunarity)
+        float lacunarity, int seed, float offset)
     {
         List<float> noiseList = new();
 
+        float max = 0;
+        float min = 0;
+
+        System.Random random = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = (random.Next(-100000, 100000) + offset);
+            float offsetY = (random.Next(-100000, 100000) + offset);
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        //clamping
         if (scale <= 0)
             scale = 0.0001f;
+        if (lacunarity < 1)
+            lacunarity = 1;
 
         foreach (Vector3Int pos in positions)
         {
@@ -101,18 +117,38 @@ public class ProceduralGeneration
 
             for (int k = 0; k < octaves; k++)
             {
-                float xCoord = (float)pos.x /*/ width**/ / (scale * frequency);
-                float yCoord = (float)pos.z /*/ height **/ / (scale * frequency);
+                float xCoord = (float)pos.x / (scale * frequency) + octaveOffsets[k].x;
+                float yCoord = (float)pos.z / (scale * frequency) + octaveOffsets[k].y;
+
+                float simplexValue = Unity.Mathematics.noise.snoise(new Unity.Mathematics.float4(xCoord, 3.0f, yCoord, 1.0f));
+                //noiseHeight += simplexValue * amplitude;
+
+                float simplex2Value = Unity.Mathematics.noise.psrnoise(new Unity.Mathematics.float2(xCoord,yCoord), new Unity.Mathematics.float2(xCoord,yCoord));
+                //noiseHeight += simplex2Value * amplitude;
 
                 float perlinValue = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
+                //noiseHeight += perlinValue * amplitude;
 
-                noiseHeight += perlinValue * amplitude;
+                float avgNoise = (simplexValue + simplexValue + perlinValue) / 3;
+                noiseHeight += avgNoise * amplitude;
 
                 amplitude *= persistance;
                 frequency *= lacunarity;
             }
 
+            //getting max and min of list
+            if (noiseHeight > max)
+                max = noiseHeight;
+            if (noiseHeight < min)
+                min = noiseHeight;
+
             noiseList.Add(noiseHeight);
+        }
+
+        //Normalizing values
+        for (int i = 0; i < noiseList.Count; i++)
+        {
+            noiseList[i] = Mathf.InverseLerp(min, max, noiseList[i]);
         }
 
         return noiseList;
@@ -235,10 +271,143 @@ public class ProceduralGeneration
         return mapDict;
     }
 
-    public static int GetRegions(Dictionary<Vector3Int, int> mapDict)
+    private static Dictionary<int, List<Vector3Int>> GetLandMasses(Dictionary<Vector3Int, int> mapDict)
     {
-        
-        
-        return 0;
+        Dictionary<int, List<Vector3Int>> landMassDict = new();
+        List<Vector3Int> checkedPositions = new();
+        Queue<Vector3Int> queue = new();
+        int landMassCount = 0;
+
+        foreach (Vector3Int pos in mapDict.Keys)
+        {
+            if (checkedPositions.Contains(pos) || mapDict[pos] == 1)
+                continue;
+
+            queue.Enqueue(pos);
+            checkedPositions.Add(pos);
+            landMassDict[landMassCount] = new List<Vector3Int>();
+
+            while (queue.Count > 0)
+            {
+                Vector3Int pos2 = queue.Dequeue();
+
+                foreach (Vector3Int neighbor in neighborsFourDirections)
+                {
+                    Vector3Int neighborCheck = pos2 + neighbor;
+
+                    if (mapDict.ContainsKey(neighborCheck) && !checkedPositions.Contains(neighborCheck))
+                    {
+                        checkedPositions.Add(neighborCheck);
+
+                        if (mapDict[neighborCheck] == 0)
+                        {
+                            landMassDict[landMassCount].Add(neighborCheck);
+                            queue.Enqueue(neighborCheck);
+                        }
+                    }
+                }
+            }
+
+            landMassCount++;
+        }
+
+        return landMassDict;
     }
+
+    public static Dictionary<Vector3Int, int> GenerateMountainRanges(Dictionary<Vector3Int, int> mainTiles, 
+        int width, int height, int mountainousPerc, int mountainRangeLength, int seed) 
+    {
+        System.Random random = new System.Random(seed.GetHashCode());
+
+        Dictionary<int, List<Vector3Int>> landMassDict = GetLandMasses(mainTiles);
+        int mapSize = width * height;
+        List<Vector3Int> mountainStarts = new();
+        
+        foreach(int key in landMassDict.Keys)
+        {
+            if (landMassDict[key].Count < mapSize / 10)
+            {
+                mountainStarts.Add(landMassDict[key][Random.Range(0, landMassDict[key].Count)]);
+                continue;
+            }
+
+            int mountainRangeCount = landMassDict[key].Count / 250;
+
+            for (int i = 0; i < mountainRangeCount; i++)
+            {
+                mountainStarts.Add(landMassDict[key][Random.Range(0, landMassDict[key].Count)]);
+            }
+        }
+
+        foreach (Vector3Int pos in mountainStarts)
+        {
+            Queue<Vector3Int> queue = new();
+            queue.Enqueue(pos);
+            
+            mainTiles[pos] = 2;
+            int step = 0;
+
+            int mainDirection = random.Next(0, 8);
+            int[] diagonals = new int[3];
+            int[] perpendiculars = new int[2];
+
+            diagonals[0] = mainDirection;
+            if (mainDirection == 0)
+                perpendiculars[0] = 6;
+            else if (mainDirection == 1)
+                perpendiculars[0] = 7;
+            else
+            perpendiculars[1] = mainDirection - 2;
+
+            if (mainDirection == 6)
+                perpendiculars[0] = 0;
+            else if (mainDirection == 7)
+                perpendiculars[0] = 1;
+            else
+                perpendiculars[0] = mainDirection + 2;
+
+            if (mainDirection == 0)
+                diagonals[1] = 7;
+            else
+                diagonals[1] = mainDirection - 1;
+
+            if (mainDirection == 7)
+                diagonals[2] = 0;
+            else
+                diagonals[2] = mainDirection + 1;
+
+
+            while (queue.Count > 0)
+            {
+                Vector3Int nextDirection = queue.Dequeue() + neighborsEightDirections[diagonals[random.Next(0,3)]];
+
+                if (mainTiles[nextDirection] == 0)
+                {
+                    int newTile = random.Next(0, 100) < mountainousPerc ? 2 : 3; //2 is hill, 3 is mountain
+                    mainTiles[nextDirection] = newTile;
+
+                    //foreach (int side in perpendiculars)
+                    //{
+
+                    //}
+
+                    if (random.Next(0, mountainRangeLength) > 0)
+                        queue.Enqueue(nextDirection);
+
+                    step++;
+                }
+
+                
+            }
+
+
+        }
+
+        return mainTiles;
+    }
+
+    //private int[] DirectionRoll()
+    //{
+    //    return int[]
+    //}
 }
