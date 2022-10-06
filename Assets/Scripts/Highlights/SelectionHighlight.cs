cd2e0d5407216aacd7d9cf2e76a077c36c80c27a
Tyler Hansen -Laptop <tuhyler@gmail.com>
2022-10-05 22:49:37 -0600

    added worker and trader animations
    Former-commit-id: 76b451fa08c633e2e2037bda6155372c313c4939
    Former-commit-id: 9c79d12fc872de33873d7828276cf33eefdc5a8d

--- Assets/Scripts/Highlights/SelectionHighlight.cs
+++ Assets/Scripts/Highlights/SelectionHighlight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,17 @@ public class SelectionHighlight : MonoBehaviour
 {
     Dictionary<MeshRenderer, Material[]> glowMaterialDictionary = new();
     Dictionary<MeshRenderer, Material[]> originalMaterialDictionary = new();
+    Dictionary<SkinnedMeshRenderer, Material[]> glowSkinnedMaterialDictionary = new();
+    Dictionary<SkinnedMeshRenderer, Material[]> originalSkinnedMaterialDictionary = new();
+
     Dictionary<Color, Material> cachedGlowColors = new();
     Dictionary<Texture, Material> cachedGlowTextures = new();
 
     [SerializeField]
-    private Material glowMaterial;
+    private Material glowMaterial; //for textures
+
+    [SerializeField]
+    private Material glowMaterialColor; //for colors
     //private bool isGlowing;
 
     private Color glowColor;
@@ -23,63 +30,66 @@ public class SelectionHighlight : MonoBehaviour
 
     private void PrepareMaterialDictionaries() //puts glowing and original materials in dictionaries at the beginning
     {
+        //first meshrenderers then skinnedmeshrenderers
         foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
         {
             Material[] originalMaterials = renderer.materials;
             originalMaterialDictionary.Add(renderer, originalMaterials);
 
-            Material[] newMaterials = new Material[renderer.materials.Length];
-            for (int i = 0; i < renderer.materials.Length; i++)
-            {
-                if (originalMaterials[i].mainTexture == null)
-                {
-                    //Material mat;
-                    if (!cachedGlowColors.TryGetValue(originalMaterials[i].color, out Material mat))
-                    {
-                        mat = new Material(glowMaterial);
-                        //By default, Unity considers a color with the property name "_Color" to be the main color
-                        mat.color = originalMaterials[i].color;
-                        cachedGlowColors[mat.color] = mat;
-                    }
-
-                    newMaterials[i] = mat;
-                    //if (!cachedGlowColors.TryGetValue(originalMaterials[i].color, out Material mat))
-                    //{
-
-                    //}
-
-                    continue;
-                } 
-                  
-                //Material mat2;
-                if (!cachedGlowTextures.TryGetValue(originalMaterials[i].mainTexture, out Material mat2))
-                {
-                    mat2 = new Material(glowMaterial);
-                    //By default, Unity considers a texture with the property name "_MainTex" to be the main texture
-                    mat2.mainTexture = originalMaterials[i].mainTexture;
-                    cachedGlowTextures[mat2.mainTexture] = mat2;
-                }
-
-                newMaterials[i] = mat2;
-
-                //if (!cachedGlowTextures.TryGetValue(originalMaterials[i].mainTexture, out Material mat2))
-                //{
-                //    mat2 = new Material(glowMaterial);
-                //    //By default, Unity considers a texture with the property name "_MainTex" to be the main texture
-                //    mat2.mainTexture = originalMaterials[i].mainTexture;
-                //    cachedGlowTextures[mat2.mainTexture] = mat2;
-                //}
-
-                //newMaterials[i] = mat2;
-            }
+            Material[] newMaterials = GetMaterialsFromMesh(originalMaterials, renderer.materials.Length);
             glowMaterialDictionary.Add(renderer, newMaterials);
+        }
+        
+        foreach (SkinnedMeshRenderer renderer in GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            Material[] originalMaterials = renderer.materials;
+            originalSkinnedMaterialDictionary.Add(renderer, originalMaterials);
+
+            Material[] newMaterials = GetMaterialsFromMesh(originalMaterials, renderer.materials.Length);
+            glowSkinnedMaterialDictionary.Add(renderer, newMaterials);
         }
     }
 
-    public void ResetGlowHighlight() //goes back to original color, not from dictionary though
+    private Material[] GetMaterialsFromMesh(Material[] originalMaterials, int materialLength)
     {
-        //if (isGlowing == false)
-        //    return;
+        Material[] newMaterials = new Material[materialLength];
+
+        for (int i = 0; i < materialLength; i++)
+        {
+            if (originalMaterials[i].mainTexture == null)
+            {
+                //Material mat;
+                if (!cachedGlowColors.TryGetValue(originalMaterials[i].color, out Material mat))
+                {
+                    mat = new Material(glowMaterialColor);
+                    //By default, Unity considers a color with the property name "_Color" to be the main color
+                    mat.color = originalMaterials[i].color;
+                    cachedGlowColors[mat.color] = mat;
+                }
+
+                newMaterials[i] = mat;
+
+                continue;
+            }
+
+            //Material mat2;
+            if (!cachedGlowTextures.TryGetValue(originalMaterials[i].mainTexture, out Material mat2))
+            {
+                mat2 = new Material(glowMaterial);
+                //By default, Unity considers a texture with the property name "_MainTex" to be the main texture
+                mat2.mainTexture = originalMaterials[i].mainTexture;
+                cachedGlowTextures[mat2.mainTexture] = mat2;
+            }
+
+            newMaterials[i] = mat2;
+        }
+
+        return newMaterials; 
+    }
+
+    public void ResetGlowHighlight() //goes back to original color
+    {
+        //first meshrenderer, then skinnedmeshrenderer
         foreach (MeshRenderer renderer in glowMaterialDictionary.Keys)
         {
             foreach (Material item in glowMaterialDictionary[renderer])
@@ -87,40 +97,16 @@ public class SelectionHighlight : MonoBehaviour
                 item.SetColor("_GlowColor", glowColor);
             }
         }
+
+        foreach (SkinnedMeshRenderer renderer in glowSkinnedMaterialDictionary.Keys)
+        {
+            foreach (Material item in glowSkinnedMaterialDictionary[renderer])
+            {
+                item.SetColor("_GlowColor", glowColor);
+            }
+        }
     }
 
-    //public void ToggleGlow(Color highlightColor) //toggles glow on or off for each item in dictionary, depending on its prevoius state
-    //{
-    //    if (!isGlowing)
-    //    {
-    //        ResetGlowHighlight();
-    //        foreach (Renderer renderer in originalMaterialDictionary.Keys)
-    //        {
-    //            renderer.materials = glowMaterialDictionary[renderer];
-    //            foreach (Material item in glowMaterialDictionary[renderer]) //sets the glow color for each item in dictionary
-    //            {
-    //                item.SetColor("_GlowColor", highlightColor);
-    //            }
-
-    //        }
-    //    }
-    //    else
-    //    {
-    //        foreach (Renderer renderer in originalMaterialDictionary.Keys)
-    //        {
-    //            renderer.materials = originalMaterialDictionary[renderer];
-    //        }
-    //    }
-    //    isGlowing = !isGlowing;
-    //}
-
-    //public void ToggleGlow(bool state, Color highlightColor) //another method that takes a boolean to specifically turn off or on glow
-    //{
-    //    if (isGlowing == state)
-    //        return;
-    //    isGlowing = !state;
-    //    ToggleGlow(highlightColor);
-    //}
 
     public void EnableHighlight(Color highlightColor) //don't like 'Toggle' using this instead
     {
@@ -134,6 +120,16 @@ public class SelectionHighlight : MonoBehaviour
             }
 
         }
+
+        foreach (SkinnedMeshRenderer renderer in originalSkinnedMaterialDictionary.Keys)
+        {
+            renderer.materials = glowSkinnedMaterialDictionary[renderer];
+            foreach (Material item in glowSkinnedMaterialDictionary[renderer]) //sets the glow color for each item in dictionary
+            {
+                item.SetColor("_GlowColor", highlightColor);
+            }
+
+        }
     }
 
     public void DisableHighlight()
@@ -141,6 +137,11 @@ public class SelectionHighlight : MonoBehaviour
         foreach (MeshRenderer renderer in originalMaterialDictionary.Keys)
         {
             renderer.materials = originalMaterialDictionary[renderer];
+        }
+
+        foreach (SkinnedMeshRenderer renderer in originalSkinnedMaterialDictionary.Keys)
+        {
+            renderer.materials = originalSkinnedMaterialDictionary[renderer];
         }
     }
 }
