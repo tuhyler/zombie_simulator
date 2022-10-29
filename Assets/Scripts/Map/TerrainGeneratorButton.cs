e9d2f8d9b714a2fbc58912396487b2cab120c4ee
Tyler Hansen -Laptop <tuhyler@gmail.com>
2022-10-28 23:10:34 -0600

    fixed ci/cd bugs

--- Assets/Scripts/Map/TerrainGeneratorButton.cs
+++ Assets/Scripts/Map/TerrainGeneratorButton.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(TerrainGenerator), true)]
-public class TerrainGeneratorButton : Editor
+//[CustomEditor(typeof(TerrainGenerator), true)]
+public class TerrainGeneratorButton
 {
     //TerrainGeneratorAbstract generator;
 
-    private void Awake()
-    {
-        //TerrainGenerator generator = (TerrainGenerator)target;
-    }
+    //private void Awake()
+    //{
+    //    //TerrainGenerator generator = (TerrainGenerator)target;
+    //}
 
-    public override void OnInspectorGUI()
-    {
-        TerrainGenerator generator = (TerrainGenerator)target;
-        //base.OnInspectorGUI();
-        if (DrawDefaultInspector())
-        {
-            if (generator.autoUpdate)
-            {
-                generator.GenerateMap();
-            }
-        }
+    //public override void OnInspectorGUI()
+    //{
+    //    TerrainGenerator generator = (TerrainGenerator)target;
+    //    //base.OnInspectorGUI();
+    //    if (DrawDefaultInspector())
+    //    {
+    //        if (generator.autoUpdate)
+    //        {
+    //            generator.GenerateMap();
+    //        }
+    //    }
 
-        if (GUILayout.Button("Generate New Map"))
-        {
-            generator.GenerateMap();
-        }
+    //    if (GUILayout.Button("Generate New Map"))
+    //    {
+    //        generator.GenerateMap();
+    //    }
 
-        if (GUILayout.Button("Remove Map"))
-        {
-            generator.RemoveMap();
-        }
-    }
+    //    if (GUILayout.Button("Remove Map"))
+    //    {
+    //        generator.RemoveMap();
+    //    }
+    //}
 }
