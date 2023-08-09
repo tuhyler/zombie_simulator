2011040edd903426209762090b50d8d2a8484fe7
TylerAsusLaptop <tuhyler@gmail.com>
2023-08-08 23:20:09 -0600

    changed upgrade format
    Former-commit-id: 6087756e6eb877d55da0705e8e7024b548416c39
    Former-commit-id: 68b2cd636992190131cb7d21697888abbd422478

--- Assets/Scripts/UI/UICityUpgradePanel.cs
+++ Assets/Scripts/UI/UICityUpgradePanel.cs
@@ -4,9 +4,10 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIResearchTooltip : MonoBehaviour
+public class UICityUpgradePanel : MonoBehaviour
 {
     public MapWorld world;
+    public CityBuilderManager cityBuilderManager;
     public TMP_Text title, level, producesTitle, health, speed, strength;
     private TMP_Text producesText;
     public Image mainImage, strengthImage;
@@ -17,10 +18,18 @@ public class UIResearchTooltip : MonoBehaviour
     private List<UIResourceInfoPanel> costsInfo = new(), producesInfo = new();
     private List<List<UIResourceInfoPanel>> consumesInfo = new();
 
+    private Vector3Int improvementLoc;
+    private CityImprovement improvement;
+    private Unit unit;
+    private bool cannotAfford;
+    private List<ResourceValue> upgradeCost = new();
+    private List<ResourceValue> produces = new();
+    List<List<ResourceValue>> consumes = new();
+    List<int> produceTime = new();
+
     //for tweening
     [HideInInspector]
     public bool activeStatus;
-    //private List<TMP_Text> noneTextList = new();
 
     private void Awake()
     {
@@ -45,15 +54,12 @@ public class UIResearchTooltip : MonoBehaviour
 
             foreach (Transform selection in produceConsumesHolders[i])
             {
-
                 //first one is for showing produces
                 if (j == 0)
                     producesInfo.Add(selection.GetComponent<UIResourceInfoPanel>());
                 //after first two is for showing consumes
                 else if (j >= 2 /*&& j <= 7*/)
                     consumesPanels.Add(selection.GetComponent<UIResourceInfoPanel>());
-                //else if (j == 8)
-                //    noneTextList.Add(selection.GetComponent<TMP_Text>());
 
                 j++;
             }
@@ -66,24 +72,57 @@ public class UIResearchTooltip : MonoBehaviour
         }
     }
 
-    public void ToggleVisibility(bool v)
+    public void ToggleVisibility(bool v, ResourceManager resourceManager = null, CityImprovement improvement = null, Unit unit = null)
     {
         if (activeStatus == v)
             return;
-
-        LeanTween.cancel(gameObject);
+        
+        activeStatus = v;
 
         if (v)
         {
             world.infoPopUpCanvas.gameObject.SetActive(true);
-            activeStatus = true;
-            gameObject.SetActive(v);
+            
+            this.improvement = improvement;
+            if (improvement != null)
+                improvementLoc = improvement.loc;
+            this.unit = unit;
+
+            string name = improvement.GetImprovementData.improvementName + '-' + improvement.GetImprovementData.improvementLevel;
+            upgradeCost = new(world.GetUpgradeCost(name));
+
+            if (unit != null)
+            {
+                UnitBuildDataSO unitData = world.GetUnitUpgradeData(name);
+                List<int> produceTime = new();
+                SetInfo(unitData.image, unitData.unitType.ToString(), unitData.unitName, unitData.unitLevel, 0, unitData.unitDescription,
+                produces, consumes, produceTime, true, unitData.health, unitData.movementSpeed, unitData.attackStrength, unitData.cargoCapacity, resourceManager);
+            }
+            else
+            {
+                ImprovementDataSO improvementData = world.GetUpgradeData(name);
+
+                consumes.Add(new(improvementData.consumedResources));
+                if (improvementData.consumedResources1.Count > 0)
+                    consumes.Add(new(improvementData.consumedResources1));
+                if (improvementData.consumedResources2.Count > 0)
+                    consumes.Add(new(improvementData.consumedResources2));
+                if (improvementData.consumedResources3.Count > 0)
+                    consumes.Add(new(improvementData.consumedResources3));
+                if (improvementData.consumedResources4.Count > 0)
+                    consumes.Add(new(improvementData.consumedResources4));
+
+                SetInfo(improvementData.image, improvementData.improvementName, improvementData.improvementDisplayName, improvementData.improvementLevel, improvementData.workEthicChange, 
+                    improvementData.improvementDescription, improvementData.producedResources, consumes, improvementData.producedResourceTime, false, 0, 0, 0, 0, resourceManager);
+            }
+
+            gameObject.SetActive(true);
 
             LeanTween.scale(allContents, Vector3.one, 0.25f).setEaseLinear();
         }
         else
         {
-            activeStatus = false;
+            ResetData();
             LeanTween.scale(allContents, Vector3.zero, 0.25f).setOnComplete(SetActiveStatusFalse);
         }
     }
@@ -94,8 +133,17 @@ public class UIResearchTooltip : MonoBehaviour
         world.infoPopUpCanvas.gameObject.SetActive(false);
     }
 
-    public void SetInfo(Vector3 position, Sprite mainSprite, string title, string displayTitle, int level, float workEthic, string description, List<ResourceValue> costs, List<ResourceValue> produces,
-        List<List<ResourceValue>> consumes, List<int> produceTimeList, bool unit, int health, float speed, int strength, int cargoCapacity)
+    private void ResetData()
+    {
+        this.improvement = null;
+        this.unit = null;
+        produces.Clear();
+        consumes.Clear();
+        produceTime.Clear();
+    }
+
+    public void SetInfo(Sprite mainSprite, string title, string displayTitle, int level, float workEthic, string description, List<ResourceValue> produces,
+        List<List<ResourceValue>> consumes, List<int> produceTimeList, bool unit, int health, float speed, int strength, int cargoCapacity, ResourceManager resourceManager)
     {
         mainImage.sprite = mainSprite;
         this.title.text = displayTitle;
@@ -105,7 +153,7 @@ public class UIResearchTooltip : MonoBehaviour
             this.level.text = "Level " + level.ToString() + " " + title;
 
         int producesCount = produces.Count;
-        int maxCount = costs.Count - 2;
+        int maxCount = upgradeCost.Count - 2;
         int resourcePanelSize = 90;
         resourcePanelSize += 0; //for gap
         int produceHolderWidth = 220;
@@ -116,7 +164,6 @@ public class UIResearchTooltip : MonoBehaviour
         //reseting produce section layout
         resourceProduceLayout.padding.top = 0;
         resourceProduceLayout.spacing = 0;
-        //int produceLayoutPadding = 10;
 
         producesTitle.text = "Produces / Requires";
 
@@ -184,14 +231,13 @@ public class UIResearchTooltip : MonoBehaviour
         if (unit)
             unitInfo.gameObject.SetActive(true);
 
-        GenerateResourceInfo(costs, costsInfo, 0); //0 means don't show produce time
+        GenerateResourceInfo(upgradeCost, costsInfo, 0, true, resourceManager); //0 means don't show produce time
 
         //adjusting height of panel
         if (producesCount > 1)
         {
             int shift = resourcePanelSize * (producesCount - 1);
             produceContentsHeight += shift;
-            //produceLayoutPadding += shift;
         }
 
         //adjusting width of panel
@@ -208,17 +254,16 @@ public class UIResearchTooltip : MonoBehaviour
         }
 
         resourceProducedHolder.sizeDelta = new Vector2(produceHolderWidth, produceHolderHeight);
-        allContents.sizeDelta = new Vector2(produceContentsWidth, 340 + produceContentsHeight);
-        //resourceProduceLayout.padding.bottom = produceLayoutPadding;
+        allContents.sizeDelta = new Vector2(produceContentsWidth, 390 + produceContentsHeight);
         imageLine.sizeDelta = new Vector2(produceContentsWidth - 20, 4);
-        //allContents.sizeDelta = new Vector2(300, 314 + produceContentsHeight); //height without produce contents window plus 70
 
         PositionCheck();
     }
 
-    private void GenerateResourceInfo(List<ResourceValue> resourcesInfo, List<UIResourceInfoPanel> resourcesToShow, int produceTime)
+    private void GenerateResourceInfo(List<ResourceValue> resourcesInfo, List<UIResourceInfoPanel> resourcesToShow, int produceTime, bool cost, ResourceManager resourceManager = null)
     {
         int resourcesCount = resourcesInfo.Count;
+        cannotAfford = false;
 
         for (int i = 0; i < resourcesToShow.Count; i++)
         {
@@ -245,9 +290,21 @@ public class UIResearchTooltip : MonoBehaviour
                 resourcesToShow[i].gameObject.SetActive(true);
 
                 resourcesToShow[i].resourceAmount.text = resourcesInfo[i].resourceAmount.ToString();
-                //resourcesToShow[i].resourceAmount.text = Mathf.RoundToInt(resourcesInfo[i].resourceAmount * (60f / produceTime)).ToString();
                 resourcesToShow[i].resourceImage.sprite = ResourceHolder.Instance.GetIcon(resourcesInfo[i].resourceType);
                 resourcesToShow[i].resourceType = resourcesInfo[i].resourceType;
+
+                if (cost)
+                {
+                    if (resourceManager.CheckResourceAvailability(resourcesInfo[i]))
+                    {
+                        resourcesToShow[i].resourceAmount.color = Color.white;
+                    }
+                    else
+                    {
+                        cannotAfford = true;
+                        resourcesToShow[i].resourceAmount.color = Color.red;
+                    }
+                }
             }
         }
     }
@@ -255,55 +312,80 @@ public class UIResearchTooltip : MonoBehaviour
     private void GenerateProduceInfo(ResourceValue producedResource, List<ResourceValue> consumedResources, int produceIndex, int produceTime)
     {
         producesInfo[produceIndex].resourceAmount.text = producedResource.resourceAmount.ToString();
-        //producesInfo[produceIndex].resourceAmount.text = Mathf.RoundToInt(producedResource.resourceAmount * (60f / produceTime)).ToString();
         producesInfo[produceIndex].resourceImage.sprite = ResourceHolder.Instance.GetIcon(producedResource.resourceType);
         producesInfo[produceIndex].resourceType = producedResource.resourceType;
 
-        //noneTextList[produceIndex].gameObject.SetActive(false);
-        //if (consumedResources.Count > 0)
-        //{
-        //    noneTextList[produceIndex].gameObject.SetActive(false);
-        //}
-        //else
-        //{
-        //    noneTextList[produceIndex].gameObject.SetActive(true);
-        //}
-
-        GenerateResourceInfo(consumedResources, consumesInfo[produceIndex], produceTime);
+        GenerateResourceInfo(consumedResources, consumesInfo[produceIndex], produceTime, false);
     }
 
     private void PositionCheck()
     {
         Vector3 p = Input.mousePosition;
-        //float x = 0.5f;
-        //float y = 0.5f;
-        //float xChange = 0;
-        //float yChange = 0;
-
         p.z = 1;
-        //p.z = 1;
-        //if (p.y + allContents.rect.height * 0.4f > Screen.height - 100)
-        //    y = 1f;
-        //else if (p.y - allContents.rect.height * 0.4f < 0)
-        //    y = 0f;
-
-        //if (p.x + allContents.rect.width * 0.5f > Screen.width)
-        //    x = 1f;
-        //else if (p.x - allContents.rect.width * 0.5f < 0)
-        //    x = 0f;
-
         allContents.pivot = new Vector2(p.x / Screen.width, p.y / Screen.height);
 
         Vector3 pos = Camera.main.ScreenToWorldPoint(p);
         allContents.transform.position = pos;
-        //if (transform.localPosition.y - rectTransform.rect.height < screenHeightNegHalf)
-        //{
-        //    allContents.pivot = new Vector2(0.5f, 0);
-        //}
-        //else
-        //{
-        //    allContents.pivot = new Vector2(0.5f, 1);
-        //}
+    }
+
+
+    public void OnPointerClick()
+    {
+        if (cannotAfford && !cityBuilderManager.isQueueing)
+        {
+            StartCoroutine(Shake());
+            UIInfoPopUpHandler.WarningMessage().Create(Input.mousePosition, "Can't afford");
+            return;
+        }
+
+        if (improvement != null)
+            cityBuilderManager.UpgradeSelectedImprovementQueueCheck(improvementLoc, improvement);
+        else if (unit != null)
+            cityBuilderManager.UpgradeUnit();
+
+        ResetData();
+        gameObject.SetActive(false);
+        world.infoPopUpCanvas.gameObject.SetActive(false);
+    }
+
+    private IEnumerator Shake()
+    {
+        Vector3 initialPos = transform.localPosition;
+        float elapsedTime = 0f;
+        float duration = 0.2f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.localPosition = initialPos + (Random.insideUnitSphere * 10f);
+            yield return null;
+        }
+
+        transform.localPosition = initialPos;
+    }
+
+    public void CheckCosts(ResourceManager resourceManager)
+    {
+        bool cannotAffordTemp = false;
+        
+        int i = 0;
+        foreach (ResourceValue value in upgradeCost)
+        {
+            if (resourceManager.CheckResourceAvailability(value))
+            {
+                costsInfo[i].resourceAmount.color = Color.white;
+            }
+            else
+            {
+                cannotAffordTemp = true;
+                costsInfo[i].resourceAmount.color = Color.red;
+            }
+
+            i++;
+
+        }
+
+        cannotAfford = cannotAffordTemp;
     }
 
     public void CloseWindow()
