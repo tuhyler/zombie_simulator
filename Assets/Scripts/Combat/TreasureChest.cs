1e7ac49e0735db6ac113335bfdd7b8c0dfa1679a
TylerAsusLaptop <tuhyler@gmail.com>
2024-03-20 23:10:34 -0600

    added treasure graphic

--- Assets/Scripts/Combat/TreasureChest.cs
+++ Assets/Scripts/Combat/TreasureChest.cs
@@ -6,4 +6,12 @@ public class TreasureChest : MonoBehaviour
 {
     [HideInInspector]
     public int amount;
+
+ //   private Animator animator;
+
+	//private void Awake()
+	//{
+	//	animator = GetComponent<Animator>();
+	//	animator.Play("Open");
+	//}
 }
