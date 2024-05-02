77962fc88cf1496799371e16dbef10338031c3e7
TylerAsusLaptop <tuhyler@gmail.com>
2024-05-01 23:04:34 -0600

    fixed some positional bugs
    Former-commit-id: 35396d6ff95c85df9aff312cad45f0ab14e2b4d9
    Former-commit-id: 2438fdf900cbf322cdd6ff9c3a5f3708a97d1055

--- Assets/Scripts/Combat/EnemyAmbush.cs
+++ Assets/Scripts/Combat/EnemyAmbush.cs
@@ -10,13 +10,17 @@ public class EnemyAmbush
     public List<Military> attackingUnits = new();
     public List<Unit> attackedUnits = new();
 
-    public EnemyAmbushData GetAmbushData()
+    public EnemyAmbushData GetAmbushData(MapWorld world)
     {
         EnemyAmbushData data = new();
 		List<UnitData> attackingList = new();
 
 		for (int i = 0; i < attackingUnits.Count; i++)
+        {
+            world.enemyCount++;
+            attackingUnits[i].id = -world.enemyCount;
 			attackingList.Add(attackingUnits[i].SaveMilitaryUnitData());
+        }
 
 		data.loc = loc;
 		data.attackingUnits = attackingList;
